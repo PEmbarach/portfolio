dea9c4e51668064d059f829e1a338ef9b3f067bc
Pablo <pabloembarach@gmail.com>
2023-12-06 10:10:38 +0000

    update about me

--- portfolio/src/About/index.js
+++ portfolio/src/About/index.js
@@ -8,18 +8,15 @@ const About = () => {
             <h1 className='about_me'>About Me</h1>
             <div>
                 <p className='info_about'>
-                I'm Pablo, Python Full Stack Developer with a strong foundation in many modern technologies, including programming languages such as Python(Flask, Django) and JavaScript(jQuery), database development(SQL, Django ORM), frontend development with CSS(Sass, Bootstrap), HTML and React.
-
-                I'm recently graduated from the Code Institute Full Stack Software Development Diploma bootcamp. 
-                I have a strong passion for learning new technologies and building web applications. 
-                I am a self-motivated, hard-working, and responsible person. I am a good team player and also able to work independently.
-
-                More than 10 years of experience in customer service, account management, sales, performance and growth marketing helps me to have a better understanding of the customer and end user, focusing on User Experience (UX). 
-                Having developed communication, data analysis and decision-making skills, in addition to having market knowledge and trends, problem-solving skills.
-
-                I'm actively seeking a Software Developer role in the tech industry where I can apply my diverse skill set. 
-                In my journey, I've developed a strong foundation in programming, but I also bring a unique perspective honed by my diverse background in customer service and marketing. 
-                I am committed to fostering innovation, building dynamic teams, and driving forward-thinking solutions.
+                I'm Pablo, a Python Full Stack Developer with expertise in Python (Flask, Django), JavaScript (jQuery), and database development (SQL, Django ORM). 
+                <br></br>
+                <br></br>
+                Recently graduated from the Code Institute Full Stack Software Development Diploma bootcamp, currently in Dublin. 
+                Passionate about learning new technologies and building web applications. Self-motivated, hard-working, and responsible. 
+                <br></br>
+                <br></br>
+                Over 10 years of experience in customer service, account management, sales, and growth marketing, providing a strong foundation in UX. 
+                Actively seeking a Software Developer role to apply my diverse skill set, combining programming with insights from a background in customer service and marketing. Committed to fostering innovation and driving forward-thinking solutions
                 </p>
             </div>
         </section>
